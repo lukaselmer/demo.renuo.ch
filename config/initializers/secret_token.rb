feb78d4f293e4ea494dbd3be6deba84161163a02
Lukas Elmer <lukas.elmer@gmail.com>
2013-10-14 20:59:30 +0200

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoRenuoCh::Application.config.secret_key_base = 'ae7e3cfe491871e4ff819a47715a7666b5078682329fa3942b6d4d3ffb2d8340573dc20b6c28a72b49b6766578b3e5d7a1cf9a244b9d9dd70eb70b6bd7f607fe'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DemoRenuoCh::Application.config.session_store :cookie_store, key: '_demo_renuo_ch_session'
