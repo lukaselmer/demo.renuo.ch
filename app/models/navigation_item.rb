b54d9372bb0d5971f053dc5bf5cfaaff697f2219
ideadapt <kunz@ideadapt.net>
2013-10-15 20:34:40 +0200

    hacky impl of link chooser for nav items. all routes with prefix _ can be chosen.

--- app/models/navigation_item.rb
+++ app/models/navigation_item.rb
@@ -1,3 +1,41 @@
 class NavigationItem < ActiveRecord::Base
   belongs_to :page
+
+  rails_admin do
+    edit do
+      field :page
+      field :target, :enum do
+        enum do
+          NavigationItem::choosable_link_options
+        end
+      end
+    end
+  end
+
+  # [I18n.t('route.user.new'),url_for(controller: 'news', action: 'new')] => <a href="users/new">Neuen Benutzer anlegen</a>
+  # [I18n.t('route.news.latest'),url_for(controller: 'news', action: 'latest')]
+  # TODO was wenn argumente benötigt werden? wie kann user diese eingeben? wie z.b. bei show ...
+  # [I18n.t('route.news.show'),url_for(controller: 'news', action: 'show', id: '??')]
+  def self.choosable_link_options
+    Rails.application.routes.routes.map do |route|
+      #{name: route.name, path: route.path.spec.to_s, controller: route.defaults[:controller], action: route.defaults[:action], method: route.constraints[:request_method]}
+
+      action = route.defaults[:action]
+      controller = route.defaults[:controller]
+      path = route.path.spec.to_s
+      method = route.constraints[:request_method]
+      [action, controller, path]
+
+      if path[/\/_\//] && "#{method}"[/GET/]
+
+        controller_names = controller.split('/')
+        label = I18n.translate("route.#{controller_names.join('_')}.#{action}")
+        # maybe use postgresql json data type :p ?
+        value = "/#{controller}##{action}"
+        [label, value]
+      end
+
+    end.compact
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ DemoRenuoCh::Application.routes.draw do
   devise_for :users, :controllers => {:registrations => "registrations"}
   mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
   root :to => "home#index"
-  resources :users
   resources :pages, only: [:show]
+
+  # generate paths that are available in cms link chooser
+  # (maybe: use cancan for authorization OR show all, auth happens on request)
+  scope path: '_' do
+    resources :users
+  end
+
 end
