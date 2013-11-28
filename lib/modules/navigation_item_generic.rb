b9d8da31af28978f8573781604227815d7f13fb7
ideadapt <kunz@ideadapt.net>
2013-11-28 14:02:15 +0100

    update navigation item target url rendering to work within (localized) apps.

--- lib/modules/navigation_item_generic.rb
+++ lib/modules/navigation_item_generic.rb
@@ -13,6 +13,20 @@ module NavigationItemGeneric
       model.order(:position)
     }
 
+    # / => /de/
+    # /de/contacts => /de/contacts
+    # /contacts => /de/contacts
+    # contacts => contacts
+    # http://www.google.com => http://www.google.com
+    def localized_target(locale)
+      t = target
+      is_l10n_app = I18n.available_locales.count > 1
+      if is_l10n_app && t.start_with?('/') && t[2] != '/' && I18n.available_locales.exclude?(t[1..2].to_sym)
+        t = "/#{locale}#{t}"
+      end
+      t
+    end
+
     model.rails_admin do
       object_label_method :label
 
