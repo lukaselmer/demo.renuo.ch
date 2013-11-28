079ef00003ad339d4a49daef360401a42e124c55
ideadapt <kunz@ideadapt.net>
2013-11-28 17:13:09 +0100

    reorder config keys

--- app/models/ckeditor/attachment_file.rb
+++ app/models/ckeditor/attachment_file.rb
@@ -3,9 +3,9 @@ class Ckeditor::AttachmentFile < Ckeditor::Asset
                     :path => '/:class/:attachment/:id_partition/:style/:filename',
                     :storage => :s3,
                     :s3_credentials => {
-                        :bucket => Figaro.env.s3_bucket_name,
-                        :access_key_id => Figaro.env.aws_access_key_id,
-                        :secret_access_key => Figaro.env.aws_secret_access_key
+                      :bucket => Figaro.env.s3_bucket_name,
+                      :access_key_id => Figaro.env.aws_access_key_id,
+                      :secret_access_key => Figaro.env.aws_secret_access_key
                     }
 
   validates_attachment_size :data, :less_than => 100.megabytes
--- app/models/ckeditor/picture.rb
+++ app/models/ckeditor/picture.rb
@@ -1,13 +1,13 @@
 class Ckeditor::Picture < Ckeditor::Asset
   has_attached_file :data,
-                    :styles => { :content => '800>', :thumb => '118x100#' },
                     :path => '/:class/:attachment/:id_partition/:style/:filename',
                     :storage => :s3,
                     :s3_credentials => {
                       :bucket => Figaro.env.s3_bucket_name,
                       :access_key_id => Figaro.env.aws_access_key_id,
                       :secret_access_key => Figaro.env.aws_secret_access_key
-                    }
+                    },
+                    :styles => { :content => '800>', :thumb => '118x100#' }
 
   validates_attachment_size :data, :less_than => 2.megabytes
   validates_attachment_presence :data
